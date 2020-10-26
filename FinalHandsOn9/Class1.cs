7c4e524d2abff891e817c0926cd155d6b61fd17e
jhilton642 <joshua.hilton@att.net>
2020-10-26 14:27:45 -0500

    Add project files.

---
+++ FinalHandsOn9/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalHandsOn9
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Shoe vans = new Shoe(11, "gray", "white", false, "white");
+            Console.WriteLine(vans.ToString());
+            Shoe work = new Shoe(11, "gray", "black", false, "black");
+            Console.WriteLine(work.ToString());
+        }
+    }
+}
---
+++ FinalHandsOn9/Shoe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FinalHandsOn9
+{
+    class Shoe
+    {
+        int size;
+        string color;
+        string secondaryColor;
+        bool openToe;
+        string laceColor;
+
+        public Shoe(int size, string color, string secondaryColor, bool openToe, string laceColor)
+        {
+            this.size = size;
+            this.color = color;
+            this.secondaryColor = secondaryColor;
+            this.openToe = openToe;
+            this.laceColor = laceColor;
+        }
+
+        public Shoe()
+        {
+        }
+
+        public int Size { get => size; set => size = value; }
+        public string Color { get => color; set => color = value; }
+        public string SecondaryColor { get => secondaryColor; set => secondaryColor = value; }
+        public bool OpenToe { get => openToe; set => openToe = value; }
+        public string LaceColor { get => laceColor; set => laceColor = value; }
+        public override string ToString()
+        {
+            return "Size: " + size + " Color: " + color + " Secondary Color: " + secondaryColor + " Open Toe: " + openToe + " Lace Color: "+laceColor;
+        }
+    }
+}
